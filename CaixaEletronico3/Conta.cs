c71c0d8970412df1ea9072b7a68a3f72726f06fd
lucasabreu13 <lucasabreu312@gmail.com>
2021-11-17 17:22:44 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEletronico3/Conta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaixaEletronico3
+{
+    class Conta
+    {
+        public int numero;
+
+        public string titular;
+
+        public double saldo;
+
+      public void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+    }
+}
---
+++ CaixaEletronico3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta lucas = new Conta();
+            Conta mario = new Conta();
+                       
+
+            lucas.saldo = 4500.0;
+            mario.saldo = 3570.0;
+
+            lucas.Saca(200);
+            MessageBox.Show("O saldo atual da conta é " + lucas.saldo);
+        }
+    }
+}
